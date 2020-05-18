48787f16f6cedc94353a4f595c7a4d74ca9cae19
Jeril L <51364591+jeril2408@users.noreply.github.com>
2020-05-19 00:16:32 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ function SearchPhotos() {
   let clientId = "mecKBIOs3ERI8h7BiptR49OJ4L8zpg1l2wNlbRktR8k";
   let query = document.getElementById("search").value;
   let url = "https://api.unsplash.com/search/photos/?client_id=" + clientId + "&query=" + query;
+  var result = allLetter(query);
+
 
   //Fetch Photos
 
@@ -15,11 +17,24 @@ function SearchPhotos() {
 
       data.results.forEach(photo => {
         let result = `
-      <img src="${photo.urls.regular}">
+      <img src="${photo.urls.regular}" style='width:100%' border="5" alt="Null">
       <a href="${photo.links.download}">
       `;
         $("#result").append(result);
       });
     });
 
+}
+
+function allLetter(query) {
+  var flag;
+  for (i = 0; i < query.length; i++) {
+    if (query[i] >= 0 && query[i] <= 9) {
+      alert("Type a valid name");
+      flag = false;
+    } 
+    else {
+      flag = true;
+    }
+  }
 }
